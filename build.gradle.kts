1ba07b8135f4601384b74da6e17a1340720207e0
PARKGE <phg5459@naver.com>
2025-02-06 18:26:07 +0900

    프로젝트 기본 세팅 (#2)
    * spring boot, JDBC, ORM 기본 세팅
    
    * 프로젝트 기본세팅
    
    * properties > yml로 교체
    
    * MainApplication -> Application으로 변경
    
    * 불필요한 줄바꿈 제거
    
    * package github.devparkge -> github.devparkge.realworld로 뎁스 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    runtimeOnly("com.h2database:h2:2.3.232")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
 }
 
 tasks.test {
