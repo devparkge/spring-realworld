6de8174bf3c0217d6ccb60d4484902365e65af5c
PARKGE <phg5459@naver.com>
2025-02-06 18:18:26 +0900

    MainApplication -> Application으로 변경

--- src/main/java/github/devparkge/Application.java
+++ src/main/java/github/devparkge/Application.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package github.devparkge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
