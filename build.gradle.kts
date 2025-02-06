f2d634fedb7b32e9b50032dbf74906647a0b1dcf
PARKGE <phg5459@naver.com>
2025-02-06 17:20:46 +0900

    spring boot, JDBC, ORM 기본 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.h2database:h2:2.3.232")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
+
 }
 
 tasks.test {
