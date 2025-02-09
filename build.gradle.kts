f02463541dda0db84e2e53ea9997deebdd62686f
PARKGE <phg5459@naver.com>
2025-02-09 20:23:42 +0900

    lombok 사용을 위해 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     runtimeOnly("com.h2database:h2:2.3.232")
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
+    compileOnly("org.projectlombok:lombok:1.18.36")
 }
 
 tasks.test {
