a3b001f05601c821dd9e000673400a4dec305553
PARKGE <phg5459@naver.com>
2025-02-10 19:58:38 +0900

    findEmail, getUserToEmail를 사용하는 로직에서 findEmail하나의 메소드만 사용하여 나온 결과값으로 비밀번호 유효성 검사를 하는 로직으로 변경
    LoginResponse의 token값을 직접 넣는 걸로 변경

--- src/main/java/github/devparkge/realworld/service/UserService.java
+++ src/main/java/github/devparkge/realworld/service/UserService.java
@@ -16,19 +16,17 @@ public class UserService {
 
     @Transactional
     public LoginResponse login(String email, String password) {
-        if (!userRepository.findEmail(email)) {
-            throw new EmailNotFoundException("유효하지 않은 이메일입니다.");
-        }
+        User user = userRepository.findEmail(email)
+                .orElseThrow(() -> new EmailNotFoundException("유효하지 않은 이메일입니다."));
 
-        User result = userRepository.getUserToEmail(email, password)
-                .orElseThrow(() -> new InvalidPasswordException("유효하지 않은 비밀번호입니다."));
+        if(!user.password().equals(password)) throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
 
         return new LoginResponse(
-                result.email(),
-                result.token(),
-                result.username(),
-                result.bio(),
-                result.image()
+                user.email(),
+                "jwt.token.here",
+                user.username(),
+                user.bio(),
+                user.image()
         );
     }
 }
