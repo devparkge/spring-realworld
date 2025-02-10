04076640fca1bc094d52db51c92d550b209b3e6a
PARKGE <phg5459@naver.com>
2025-02-10 17:47:54 +0900

    UserMapper 구현체를 의존하던 코드를 UserRepository 인터페이스를 의존하게 변경

--- src/main/java/github/devparkge/realworld/service/UserService.java
+++ src/main/java/github/devparkge/realworld/service/UserService.java
@@ -3,6 +3,7 @@ package github.devparkge.realworld.service;
 import github.devparkge.realworld.controller.response.LoginResponse;
 import github.devparkge.realworld.exception.EmailNotFoundException;
 import github.devparkge.realworld.exception.InvalidPasswordException;
+import github.devparkge.realworld.repository.UserRepository;
 import github.devparkge.realworld.repository.mapper.UserMapper;
 import github.devparkge.realworld.service.dto.SelectUserToPasswordDto;
 import lombok.AllArgsConstructor;
@@ -12,15 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @AllArgsConstructor
 public class UserService {
-    private final UserMapper userMapper;
+    private final UserRepository userRepository;
 
     @Transactional
     public LoginResponse login(String email, String password) {
-        if(!userMapper.selectEmail(email)) {
+        if(!userRepository.selectEmail(email)) {
             throw new EmailNotFoundException("유효하지 않은 이메일입니다.");
         }
 
-        SelectUserToPasswordDto result = userMapper.selectUserToPassword(email,password);
+        SelectUserToPasswordDto result = userRepository.selectUserToPassword(email,password);
         if(result == null) {
             throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
         }
