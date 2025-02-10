f7589219634de9afa0382f17349e38911c5876fa
PARKGE <phg5459@naver.com>
2025-02-10 20:27:52 +0900

    validatePassword메소드 추가로 리팩토링

--- src/main/java/github/devparkge/realworld/service/UserService.java
+++ src/main/java/github/devparkge/realworld/service/UserService.java
@@ -1,10 +1,10 @@
 package github.devparkge.realworld.service;
 
 import github.devparkge.realworld.controller.response.LoginResponse;
+import github.devparkge.realworld.domain.model.User;
+import github.devparkge.realworld.domain.repository.UserRepository;
 import github.devparkge.realworld.exception.EmailNotFoundException;
 import github.devparkge.realworld.exception.InvalidPasswordException;
-import github.devparkge.realworld.domain.repository.UserRepository;
-import github.devparkge.realworld.domain.model.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,8 +19,14 @@ public class UserService {
         User user = userRepository.findEmail(email)
                 .orElseThrow(() -> new EmailNotFoundException("유효하지 않은 이메일입니다."));
 
-        if(!user.machesPassword(email)) throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
+        validatePassword(user, password);
 
         return LoginResponse.from(user);
+    }
+
+    private void validatePassword(User user, String password) {
+        if (!user.machesPassword(password)) {
+            throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
+        }
     }
 }
