9e1c662e0072ee38515f87cb6f0820caee62d599
PARKGE <phg5459@naver.com>
2025-02-10 18:49:31 +0900

    불필요한 주석 제거

--- src/main/java/github/devparkge/realworld/repository/UserRepository.java
+++ src/main/java/github/devparkge/realworld/repository/UserRepository.java
@@ -5,9 +5,7 @@ import github.devparkge.realworld.repository.domain.User;
 import java.util.Optional;
 
 public interface UserRepository {
-    //Login email 검증
     boolean findEmail(String email);
 
-    //Login password 확인
     Optional<User> getUserToEmail(String email, String password);
 }
