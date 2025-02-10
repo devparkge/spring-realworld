e054946af950b5b6c1962064a12b6b7416a0318b
PARKGE <phg5459@naver.com>
2025-02-10 19:56:35 +0900

    getUserToEmail 메소드 제거 findEmail 리턴타입 Optional<User>로 변경

--- src/main/java/github/devparkge/realworld/repository/UserRepository.java
+++ src/main/java/github/devparkge/realworld/repository/UserRepository.java
@@ -5,7 +5,5 @@ import github.devparkge.realworld.repository.domain.User;
 import java.util.Optional;
 
 public interface UserRepository {
-    boolean findEmail(String email);
-
-    Optional<User> getUserToEmail(String email, String password);
+    Optional<User> findEmail(String email);
 }
