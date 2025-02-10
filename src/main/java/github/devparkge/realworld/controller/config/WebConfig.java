4c5d35fadf155fac0d94972532ecf0cfea5ecd84
PARKGE <phg5459@naver.com>
2025-02-10 19:51:15 +0900

    @Configuration 어노테이션 추가

--- src/main/java/github/devparkge/realworld/controller/config/WebConfig.java
+++ src/main/java/github/devparkge/realworld/controller/config/WebConfig.java
@@ -1,11 +1,13 @@
 package github.devparkge.realworld.controller.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final ObjectMapper objectMapper;
 
