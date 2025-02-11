4d40aea5c79b58937c616f7107380fbd68a83a1c
PARKGE <phg5459@naver.com>
2025-02-12 01:40:19 +0900

    jjwt 의존성 부여 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,9 @@ dependencies {
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.4")
 
     // ✅ JSON Web Token (JWT)
-    implementation("com.auth0:java-jwt:4.5.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     // ✅ Lombok (컴파일 시에만 필요)
     compileOnly("org.projectlombok:lombok:1.18.36")
