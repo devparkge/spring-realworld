87cf4def672d4be2e7d6cc3fe2d8a4e57dc9646e
PARKGE <phg5459@naver.com>
2025-02-14 01:51:44 +0900

    jwt 검증 필터 추가

---
+++ src/main/java/github/devparkge/realworld/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package github.devparkge.realworld.filter;
+
+
+import github.devparkge.realworld.service.UserService;
+import github.devparkge.realworld.util.JwtUtil;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+
+public class JwtAuthenticationFilter implements Filter {
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserService userService) {
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String header = ((HttpServletRequest) request).getHeader("Authorization");
+            String token = (header != null && header.startsWith("Bearer ")) ? header.substring(7) : null;
+            String email = jwtUtil.parseToken(token);
+            if (userService.jwtAuthenticationByEmail(email)) {
+                request.setAttribute("email", email);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
