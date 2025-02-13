137782d53ca9b2bbf5b12f1d5270e721d400c330
PARKGE <phg5459@naver.com>
2025-02-14 03:26:03 +0900

    get current user get mapping

--- src/main/java/github/devparkge/realworld/controller/api/UserApiController.java
+++ src/main/java/github/devparkge/realworld/controller/api/UserApiController.java
@@ -2,10 +2,15 @@ package github.devparkge.realworld.controller.api;
 
 import github.devparkge.realworld.controller.config.JsonRequest;
 import github.devparkge.realworld.controller.request.LoginRequest;
+import github.devparkge.realworld.controller.response.CurrentUserResponse;
 import github.devparkge.realworld.controller.response.LoginResponse;
+import github.devparkge.realworld.exception.InvalidTokenException;
 import github.devparkge.realworld.service.UserService;
+import github.devparkge.realworld.service.dto.GetCurrentUserDto;
 import github.devparkge.realworld.service.dto.LoginDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +30,18 @@ public class UserApiController {
                 loginRequest.password()
         );
         return LoginResponse.from(loginDto);
+    }
+
+    @GetMapping
+    public CurrentUserResponse currentUser(
+            HttpServletRequest request
+    ) {
+        String header = ((HttpServletRequest) request).getHeader("Authorization");
+        String token = (header != null && header.startsWith("Bearer ")) ? header.substring(7) : null;
+        String email = (String) request.getAttribute("email");
+        if(email == null) throw new InvalidTokenException("유효하지 않은 토큰입니다.");
+        GetCurrentUserDto user = userService.getCurrentUserDto(email, token);
+
+        return CurrentUserResponse.from(user);
     }
 }
