917afc668a9c3d4e73526e18b95a1f2884be7418
PARKGE <phg5459@naver.com>
2025-02-17 18:42:20 +0900

    @JsonTypeInfo(include = WRAPPER_ARRAY, use = NAME) > @JsonTypeInfo(include = WRAPPER_OBJECT, use = NAME)

--- src/main/java/github/devparkge/realworld/controller/response/SignUpResponse.java
+++ src/main/java/github/devparkge/realworld/controller/response/SignUpResponse.java
@@ -5,10 +5,11 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import github.devparkge.realworld.service.dto.SignUpDto;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_ARRAY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_OBJECT;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 @JsonTypeName("user")
-@JsonTypeInfo(include = WRAPPER_ARRAY, use = NAME)
+@JsonTypeInfo(include = WRAPPER_OBJECT, use = NAME)
 public record SignUpResponse(
         String email,
         String token,
