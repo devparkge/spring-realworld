c3ba7f04fc3bceaf798958c0fe48af3e21e2f750
PARKGE <phg5459@naver.com>
2025-02-17 17:19:18 +0900

    Update User 비즈니스 로직 추가

--- src/main/java/github/devparkge/realworld/service/UserService.java
+++ src/main/java/github/devparkge/realworld/service/UserService.java
@@ -8,11 +8,13 @@ import github.devparkge.realworld.exception.InvalidPasswordException;
 import github.devparkge.realworld.exception.UUIDNotFoundException;
 import github.devparkge.realworld.service.dto.LoginDto;
 import github.devparkge.realworld.service.dto.SignUpDto;
+import github.devparkge.realworld.service.dto.UpdateUserDto;
 import github.devparkge.realworld.util.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -51,7 +53,34 @@ public class UserService {
     @Transactional
     public SignUpDto signUp(String username, String email, String password) {
         if (userRepository.findByEmail(email).isPresent()) throw new DuplicateEmailException("중복된 이메일 입니다.");
+        User user = userRepository.saveUser(username, email, password);
+        String token = jwtUtil.generateToken(user.uuid());
 
-        return SignUpDto.from(userRepository.saveUser(username, email, password));
+        return SignUpDto.from(user, token);
+    }
+
+    public UpdateUserDto updateUser(
+            UUID uuid,
+            Optional<String> email,
+            Optional<String> username,
+            Optional<String> password,
+            Optional<String> bio,
+            Optional<String> image
+    ) {
+        String token = jwtUtil.generateToken(uuid);
+        User user = userRepository.findByUUID(uuid)
+                .orElseThrow(() -> new UUIDNotFoundException("존재하지 않는 아이디입니다."));
+        User newUser = User.updateUser(user, email, username, password, bio, image);
+        System.out.println("user : " + newUser);
+
+        return UpdateUserDto.from(
+                userRepository.updateUser(
+                uuid,
+                newUser.email(),
+                newUser.username(),
+                newUser.password(),
+                newUser.bio(),
+                newUser.image()
+        ), token);
     }
 }
