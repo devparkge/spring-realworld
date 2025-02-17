261d19e48ef9fd8d93f3c5df9cc06d6847fe5134
PARKGE <phg5459@naver.com>
2025-02-17 17:13:00 +0900

    token 값 주입

--- src/main/java/github/devparkge/realworld/service/dto/SignUpDto.java
+++ src/main/java/github/devparkge/realworld/service/dto/SignUpDto.java
@@ -2,6 +2,7 @@ package github.devparkge.realworld.service.dto;
 
 import github.devparkge.realworld.domain.model.User;
 
+
 public record SignUpDto(
         String email,
         String token,
@@ -9,10 +10,10 @@ public record SignUpDto(
         String bio,
         String image
 ) {
-    public static SignUpDto from(User user) {
+    public static SignUpDto from(User user, String token) {
         return new SignUpDto(
                 user.email(),
-                "",
+                token,
                 user.username(),
                 user.bio(),
                 user.image()
