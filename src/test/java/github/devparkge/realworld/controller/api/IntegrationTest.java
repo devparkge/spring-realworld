0f46c882950a97492e8bf22d40b3e5d0e7de766a
PARKGE <phg5459@naver.com>
2025-02-19 01:32:01 +0900

    ProfilesApiController 테스트 코드 추가

--- src/test/java/github/devparkge/realworld/controller/api/IntegrationTest.java
+++ src/test/java/github/devparkge/realworld/controller/api/IntegrationTest.java
@@ -1,7 +1,9 @@
 package github.devparkge.realworld.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import github.devparkge.realworld.domain.model.Follower;
 import github.devparkge.realworld.domain.model.User;
+import github.devparkge.realworld.domain.repository.InMemoryFollowerRepository;
 import github.devparkge.realworld.domain.repository.InMemoryUserRepository;
 import github.devparkge.realworld.util.JwtUtil;
 import org.junit.jupiter.api.AfterEach;
@@ -10,11 +12,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IntegrationTest {
     @Autowired
     private InMemoryUserRepository userRepository;
+    @Autowired
+    private InMemoryFollowerRepository followerRepository;
     @Autowired
     protected MockMvc mockMvc;
     @Autowired
@@ -25,13 +31,21 @@ public class IntegrationTest {
     @AfterEach
     void after() {
         this.userRepository.clear();
+        this.followerRepository.clear();
     }
 
     protected User createUser(String username, String email, String password) {
         return this.userRepository.saveUser(
-                "parkge",
-                "parkge@gmail.com",
-                "1234"
+                username,
+                email,
+                password
+        );
+    }
+
+    protected Follower createFollower(String username, UUID uuid) {
+        return this.followerRepository.follow(
+                username,
+                uuid
         );
     }
 }
---
+++ src/test/java/github/devparkge/realworld/controller/api/ProfilesApiControllerTest.java
@@ -0,0 +1,71 @@
+package github.devparkge.realworld.controller.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@DisplayName("ProfilesApiController 통합 테스트")
+class ProfilesApiControllerTest extends IntegrationTest {
+    @Nested
+    @DisplayName("GET /api/profiles/:username")
+    class GetProfile {
+        @Test
+        @DisplayName("인증된 유저의 요청에 대한 username의 프로필을 반환")
+        void authenticGetProfilse() throws Exception {
+            var targetUser = createUser(
+                    "gunKim",
+                    "gunkim@gamil.com",
+                    "1234"
+            );
+            var myUser = createUser(
+                    "parkge",
+                    "parkge@gamil.com",
+                    "1234"
+            );
+            var folllower = createFollower(
+                    targetUser.username(),
+                    myUser.uuid()
+            );
+
+            String token = "Bearer " + jwtUtil.generateToken(myUser.uuid());
+            mockMvc.perform(get("/api/profiles/gunKim")
+                            .header(HttpHeaders.AUTHORIZATION,token))
+                    .andExpect(jsonPath("$.profile.username").value(targetUser.username()))
+                    .andExpect(jsonPath("$.profile.bio").isEmpty())
+                    .andExpect(jsonPath("$.profile.image").isEmpty())
+                    .andExpect(jsonPath("$.profile.following").value(true))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("인증되지 않은 유저의 요청에 대한 username의 프로필을 반환")
+        void noGetProfilse() throws Exception {
+            var targetUser = createUser(
+                    "gunKim",
+                    "gunkim@gamil.com",
+                    "1234"
+            );
+            var myUser = createUser(
+                    "parkge",
+                    "parkge@gamil.com",
+                    "1234"
+            );
+            var folllower = createFollower(
+                    targetUser.username(),
+                    myUser.uuid()
+            );
+
+            mockMvc.perform(get("/api/profiles/gunKim"))
+                    .andExpect(jsonPath("$.profile.username").value(targetUser.username()))
+                    .andExpect(jsonPath("$.profile.bio").isEmpty())
+                    .andExpect(jsonPath("$.profile.image").isEmpty())
+                    .andExpect(jsonPath("$.profile.following").value(false))
+                    .andDo(print());
+        }
+    }
+}
