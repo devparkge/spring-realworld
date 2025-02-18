337b1b6b92d97b07a71d5ff7de2b872642a7ead6
PARKGE <phg5459@naver.com>
2025-02-18 23:15:02 +0900

    exception 추가

---
+++ src/main/java/github/devparkge/realworld/exception/UsernameNotFoundException.java
@@ -0,0 +1,7 @@
+package github.devparkge.realworld.exception;
+
+public class UsernameNotFoundException extends RuntimeException {
+    public UsernameNotFoundException(String message) {
+        super(message);
+    }
+}
