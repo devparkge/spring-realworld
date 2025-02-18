c9a721f9bef524ad93388f11b80488169b2088d8
PARKGE <phg5459@naver.com>
2025-02-19 01:28:35 +0900

    isFollow, follow 메소드 구현

--- src/main/java/github/devparkge/realworld/domain/repository/FollowerRepository.java
+++ src/main/java/github/devparkge/realworld/domain/repository/FollowerRepository.java
@@ -1,7 +1,10 @@
 package github.devparkge.realworld.domain.repository;
 
+import github.devparkge.realworld.domain.model.Follower;
+
 import java.util.UUID;
 
 public interface FollowerRepository {
     boolean isFollow(String username, UUID uuid);
+    Follower follow(String username, UUID uuid);
 }
--- src/main/java/github/devparkge/realworld/domain/repository/InMemoryFollowerRepository.java
+++ src/main/java/github/devparkge/realworld/domain/repository/InMemoryFollowerRepository.java
@@ -8,12 +8,26 @@ import java.util.List;
 import java.util.UUID;
 
 @Component
-public class InMemoryFollowerRepository implements FollowerRepository{
+public class InMemoryFollowerRepository implements FollowerRepository {
     public List<Follower> followers = new ArrayList<>();
 
     @Override
     public boolean isFollow(String username, UUID uuid) {
-        return false;
+        return followers.stream()
+                .allMatch(
+                        follower ->
+                                follower.uuid().equals(uuid) && follower.username().equals(username)
+                );
+
+    }
+
+    @Override
+    public Follower follow(String username, UUID uuid) {
+        return Follower.follow(
+                UUID.randomUUID(),
+                uuid,
+                username
+        );
     }
 
     public void clear() {
