e3c764dcb59da30c37054f0da795ebe7218f7104
PARKGE <phg5459@naver.com>
2025-02-21 01:51:55 +0900

    불필요한 코드 삭제

--- src/main/java/github/devparkge/realworld/domain/user/service/UsersService.java
+++ src/main/java/github/devparkge/realworld/domain/user/service/UsersService.java
@@ -3,9 +3,6 @@ package github.devparkge.realworld.domain.user.service;
 import github.devparkge.realworld.domain.user.model.User;
 import github.devparkge.realworld.domain.user.repository.UserRepository;
 import github.devparkge.realworld.exception.DuplicateEmailException;
-import github.devparkge.realworld.exception.EmailNotFoundException;
-import github.devparkge.realworld.exception.InvalidPasswordException;
-import github.devparkge.realworld.util.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,21 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class UsersService {
     private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
-
-    @Transactional
-    public User login(String email, String password) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new EmailNotFoundException("유효하지 않은 이메일입니다."));
-        validatePassword(user, password);
-        return user;
-    }
-
-    private void validatePassword(User user, String password) {
-        if (!user.matchesPassword(password)) {
-            throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
-        }
-    }
 
     @Transactional
     public User signUp(String username, String email, String password) {
