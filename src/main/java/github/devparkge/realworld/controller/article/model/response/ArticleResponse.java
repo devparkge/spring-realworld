66c644ef35f2c2e7d5bd809a972944ed3a191f3a
PARKGE <phg5459@naver.com>
2025-02-23 03:27:45 +0900

    @JsonTypeName 제거

--- src/main/java/github/devparkge/realworld/controller/article/model/response/ArticleResponse.java
+++ src/main/java/github/devparkge/realworld/controller/article/model/response/ArticleResponse.java
@@ -1,20 +1,12 @@
 package github.devparkge.realworld.controller.article.model.response;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import github.devparkge.realworld.controller.article.model.Author;
-import github.devparkge.realworld.controller.user.model.response.ProfileResponse;
 import github.devparkge.realworld.domain.article.model.Article;
 import github.devparkge.realworld.domain.user.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_OBJECT;
-import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
-
-@JsonTypeName("article")
-@JsonTypeInfo(include = WRAPPER_OBJECT, use = NAME)
 public record ArticleResponse(
         String slug,
         String title,
