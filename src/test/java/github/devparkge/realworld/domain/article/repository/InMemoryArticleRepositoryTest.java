d8359795b7f708727b2c856f11a67f7f57d88731
PARKGE <phg5459@naver.com>
2025-02-23 03:29:34 +0900

    의존성 추가

--- src/test/java/github/devparkge/realworld/domain/article/repository/InMemoryArticleRepositoryTest.java
+++ src/test/java/github/devparkge/realworld/domain/article/repository/InMemoryArticleRepositoryTest.java
@@ -1,17 +1,30 @@
 package github.devparkge.realworld.domain.article.repository;
 
 import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.UUID;
 
 
 class InMemoryArticleRepositoryTest {
+    @Autowired
+    private ArticleTagRepository articleTagRepository;
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void saveArticle() {
-        var repository = new InMemoryArticleRepository();
+        var repository = new InMemoryArticleRepository(
+                articleTagRepository,
+                tagRepository,
+                userRepository
+        );
         UUID userId = UUID.randomUUID();
         Article article = repository.saveArticle(
                 userId,
@@ -20,7 +33,6 @@ class InMemoryArticleRepositoryTest {
                 "test saveArticle unit test",
                 List.of("unitTest", "test", "unit")
         );
-
         Assertions.assertThat(article.userId().equals(article.userId()));
         Assertions.assertThat(article.title().equals(article.title()));
         Assertions.assertThat(article.description().equals(article.description()));
