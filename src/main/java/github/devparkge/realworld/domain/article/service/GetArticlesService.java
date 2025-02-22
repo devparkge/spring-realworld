958717e88b96a0d5ffba417738b8d52f1e8c4655
PARKGE <phg5459@naver.com>
2025-02-23 03:25:43 +0900

    getArticles 비즈니스로직 추가

---
+++ src/main/java/github/devparkge/realworld/domain/article/service/GetArticlesService.java
@@ -0,0 +1,18 @@
+package github.devparkge.realworld.domain.article.service;
+
+import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetArticlesService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> getArticles(String tag, String author, int limit, int offset) {
+        return articleRepository.findByTagOrAuthor(tag, author, limit, offset);
+    }
+}
