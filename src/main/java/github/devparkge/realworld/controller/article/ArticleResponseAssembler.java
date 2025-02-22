a7a3ab005ecd5b47d3575228dc65e8bcefb1b626
PARKGE <phg5459@naver.com>
2025-02-23 03:27:10 +0900

    ArticleResponse Assembler 추가

---
+++ src/main/java/github/devparkge/realworld/controller/article/ArticleResponseAssembler.java
@@ -0,0 +1,39 @@
+package github.devparkge.realworld.controller.article;
+
+import github.devparkge.realworld.controller.article.model.response.ArticleResponse;
+import github.devparkge.realworld.controller.article.model.response.wrapper.ArticleWrapper;
+import github.devparkge.realworld.controller.article.model.response.wrapper.ArticlesWrapper;
+import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.user.model.User;
+import github.devparkge.realworld.domain.user.service.FollowService;
+import github.devparkge.realworld.domain.user.service.GetUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleResponseAssembler {
+    private final GetUserService getUserService;
+    private final FollowService followService;
+
+    public ArticleWrapper assembleArticleResponse(Article article, UUID authUserUUID) {
+        User user = getUserService.getByUUID(authUserUUID);
+        boolean isFollowing = (authUserUUID != null) ? followService.isFollowing(user.username(), authUserUUID) : false;
+        return ArticleWrapper.create(ArticleResponse.from(article, user, isFollowing));
+    }
+
+    public ArticlesWrapper assembleArticlesResponse(List<Article> articles, UUID authUserUUID) {
+        return ArticlesWrapper.create(
+                articles.stream()
+                        .map(article -> {
+                                    User user = getUserService.getByUUID(article.userId());
+                                    boolean isFollowing = (authUserUUID != null) ? followService.isFollowing(user.username(), authUserUUID) : false;
+                                    return ArticleResponse.from(article, user, isFollowing);
+                                }
+                        ).toList()
+        );
+    }
+}
