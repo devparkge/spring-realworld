721eb683cff145f89de3119f1e3e6fd130f20f45
PARKGE <phg5459@naver.com>
2025-02-26 00:46:43 +0900

    findBySlug 추가

--- src/main/java/github/devparkge/realworld/domain/article/repository/ArticleReadRepository.java
+++ src/main/java/github/devparkge/realworld/domain/article/repository/ArticleReadRepository.java
@@ -1,12 +1,15 @@
 package github.devparkge.realworld.domain.article.repository;
 
 import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.article.model.Slug;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ArticleReadRepository {
     List<Article> findByTagAndAuthorAndFavorited(String tagName, String author, String favorited, int limit, int offset);
     List<UUID> getFavoritesArticleIds(UUID userId);
+    Optional<Article> findBySlug(Slug slug);
     int getCountByArticleId(UUID articleId);
 }
