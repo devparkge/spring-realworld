37bac289dc1b099b798d21ce046d06d4f9c77a40
PARKGE <phg5459@naver.com>
2025-02-25 22:33:39 +0900

    RequestParm > ModelAttribute로 변경 request DTO 추가

--- src/main/java/github/devparkge/realworld/controller/article/api/ArticlesApiController.java
+++ src/main/java/github/devparkge/realworld/controller/article/api/ArticlesApiController.java
@@ -5,6 +5,7 @@ import github.devparkge.realworld.config.annotation.JwtAuthenticationOptional;
 import github.devparkge.realworld.config.annotation.JwtAuthenticationRequired;
 import github.devparkge.realworld.controller.article.ArticleResponseAssembler;
 import github.devparkge.realworld.controller.article.model.request.CreateArticleRequest;
+import github.devparkge.realworld.controller.article.model.request.GetArticlesRequest;
 import github.devparkge.realworld.controller.article.model.response.wrapper.ArticleWrapper;
 import github.devparkge.realworld.controller.article.model.response.wrapper.ArticlesWrapper;
 import github.devparkge.realworld.domain.article.model.Article;
@@ -42,13 +43,15 @@ public class ArticlesApiController {
     @GetMapping
     public ArticlesWrapper getArticles(
             @JwtAuthenticationOptional UUID authUserUUID,
-            @RequestParam(required = false, name = "tag") String tag,
-            @RequestParam(required = false, name = "author") String author,
-            @RequestParam(required = false, name = "favorited") String favorited,
-            @RequestParam(defaultValue = "20", name = "limit") int limit,
-            @RequestParam(defaultValue = "0", name = "offset") int offset
+            @ModelAttribute GetArticlesRequest getArticlesRequest
     ) {
-        List<Article> articles = getArticlesService.getArticles(tag, author, favorited, limit, offset);
+        List<Article> articles = getArticlesService.getArticles(
+                getArticlesRequest.tag(),
+                getArticlesRequest.author(),
+                getArticlesRequest.favorited(),
+                getArticlesRequest.limit(),
+                getArticlesRequest.offset()
+        );
         return articleResponseAssembler.assembleArticlesResponse(articles, authUserUUID);
     }
 }
