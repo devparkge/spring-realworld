3b470d5c69015f0efe3ceb041a779cb32dba095c
PARKGE <phg5459@naver.com>
2025-02-26 23:53:38 +0900

    서비스 로직 수정

--- src/main/java/github/devparkge/realworld/domain/article/service/DeleteArticleService.java
+++ src/main/java/github/devparkge/realworld/domain/article/service/DeleteArticleService.java
@@ -1,9 +1,12 @@
 package github.devparkge.realworld.domain.article.service;
 
+import github.devparkge.realworld.domain.article.model.Article;
 import github.devparkge.realworld.domain.article.model.Slug;
 import github.devparkge.realworld.domain.article.repository.ArticleRepository;
 import github.devparkge.realworld.domain.user.model.User;
 import github.devparkge.realworld.domain.user.repository.UserRepository;
+import github.devparkge.realworld.exception.NotArticleAuthorException;
+import github.devparkge.realworld.exception.SlugNotFoundException;
 import github.devparkge.realworld.exception.UUIDNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,8 +19,12 @@ public class DeleteArticleService {
     private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
 
-    public void deleteArticle(Slug slug, UUID authUserId) {
-        User authenticatedUser = userRepository.findByUUID(authUserId).orElseThrow(() -> new UUIDNotFoundException(String.format("%s은 존재하지 않습니다.", authUserId)));
-        articleRepository.delete(slug, authenticatedUser);
+    public void deleteArticle(Slug slug, UUID authUserUUID) {
+        Article article = articleRepository.findBySlug(slug).orElseThrow(() -> new SlugNotFoundException(String.format("%s은 존재하지 않습니다.", slug.value())));
+        User authenticatedUser = userRepository.findByUUID(authUserUUID).orElseThrow(() -> new UUIDNotFoundException(String.format("%s은 존재하지 않습니다.", authUserUUID)));
+        if(!article.author().uuid().equals(authenticatedUser.uuid())) {
+            throw new NotArticleAuthorException(String.format("%s은 %s와 일치하는 author가 아닙니다.", authenticatedUser.uuid(), article.author().uuid()));
+        }
+        articleRepository.delete(article);
     }
 }
