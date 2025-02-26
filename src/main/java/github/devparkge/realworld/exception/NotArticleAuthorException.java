9cd32e49a2987829d8cdd171f7504f9be446b742
PARKGE <phg5459@naver.com>
2025-02-26 23:52:40 +0900

    excption 추가

---
+++ src/main/java/github/devparkge/realworld/exception/NotArticleAuthorException.java
@@ -0,0 +1,7 @@
+package github.devparkge.realworld.exception;
+
+public class NotArticleAuthorException extends RuntimeException {
+    public NotArticleAuthorException(String message) {
+        super(message);
+    }
+}
