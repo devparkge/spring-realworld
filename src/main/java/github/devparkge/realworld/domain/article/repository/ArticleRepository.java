00101d8e5c31a5b5e5d2038a4d27066ed58f91ed
PARKGE <phg5459@naver.com>
2025-02-26 23:28:42 +0900

    ArticleRepository delete 추가 및 구현체 구현

--- src/main/java/github/devparkge/realworld/domain/article/repository/ArticleRepository.java
+++ src/main/java/github/devparkge/realworld/domain/article/repository/ArticleRepository.java
@@ -1,7 +1,10 @@
 package github.devparkge.realworld.domain.article.repository;
 
 import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.article.model.Slug;
+import github.devparkge.realworld.domain.user.model.User;
 
 public interface ArticleRepository extends ArticleReadRepository {
     Article save(Article article);
+    void delete(Slug slug, User authenticatedUser);
 }
--- src/main/java/github/devparkge/realworld/infrastructure/article/repository/InMemoryArticleRepository.java
+++ src/main/java/github/devparkge/realworld/infrastructure/article/repository/InMemoryArticleRepository.java
@@ -1,10 +1,14 @@
 package github.devparkge.realworld.infrastructure.article.repository;
 
 import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.article.model.Slug;
 import github.devparkge.realworld.domain.article.repository.ArticleRepository;
+import github.devparkge.realworld.domain.user.model.User;
 import github.devparkge.realworld.domain.user.repository.UserRepository;
 import github.devparkge.realworld.infrastructure.article.model.ArticlePersistence;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class InMemoryArticleRepository extends InMemoryArticleReadRepository implements ArticleRepository {
@@ -16,5 +20,13 @@ public class InMemoryArticleRepository extends InMemoryArticleReadRepository imp
     public Article save(Article article) {
         articles.put(article.uuid(), ArticlePersistence.from(article));
         return article;
+    }
+
+    @Override
+    public void delete(Slug slug, User authenticatedUser) {
+        findAll()
+                .filter(article -> article.author().equals(authenticatedUser))
+                .filter(article -> article.slug().value().equals(slug.value()))
+                .forEach(article -> articles.remove(article.uuid()));
     }
 }
