6615294ee4c154bef37b749d0914fcdc0b7bf1ac
PARKGE <phg5459@naver.com>
2025-02-27 23:32:52 +0900

    favorite 추가

--- src/main/java/github/devparkge/realworld/domain/article/repository/ArticleRepository.java
+++ src/main/java/github/devparkge/realworld/domain/article/repository/ArticleRepository.java
@@ -1,9 +1,12 @@
 package github.devparkge.realworld.domain.article.repository;
 
 import github.devparkge.realworld.domain.article.model.Article;
+import github.devparkge.realworld.domain.user.model.User;
 
 public interface ArticleRepository extends ArticleReadRepository {
     Article save(Article article);
 
     void delete(Article article);
+
+    void  favorite(Article article, User user);
 }
