0a07562d84834df1534acea27009538f8b71ff9a
PARKGE <phg5459@naver.com>
2025-03-02 23:43:07 +0900

    List<Comment> > Map<Integer, Comment> 변경 idIncrement auto increment를 위한 변수 추가 save 로직 수정 clear메소드 추가

--- src/main/java/github/devparkge/realworld/infrastructure/comment/repository/InMemoryCommentRepository.java
+++ src/main/java/github/devparkge/realworld/infrastructure/comment/repository/InMemoryCommentRepository.java
@@ -5,17 +5,25 @@ import github.devparkge.realworld.domain.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
 public class InMemoryCommentRepository implements CommentRepository {
-    private List<Comment> comments = new ArrayList<>();
+    private Map<Integer, Comment> comments = new HashMap<>();
+    private AtomicInteger idIncrement = new AtomicInteger(1);
 
     @Override
     public Comment save(Comment comment) {
-        comments.add(comment);
-        return comment;
+        int id = (comment.id() == -1) ? idIncrement.getAndIncrement() : comment.id();
+        comments.put(id, comment.updateId(id));
+        return comments.get(id);
+    }
+
+    public void clear() {
+        comments = new HashMap<>();
+        idIncrement = new AtomicInteger(1);
     }
 }
