b17ff7cd07ae544ed1429e4d8e971f71be6d01a1
PARKGE <phg5459@naver.com>
2025-03-02 23:41:05 +0900

    create, updateId 메소드 추가

--- src/main/java/github/devparkge/realworld/domain/comment/model/Comment.java
+++ src/main/java/github/devparkge/realworld/domain/comment/model/Comment.java
@@ -2,9 +2,11 @@ package github.devparkge.realworld.domain.comment.model;
 
 import github.devparkge.realworld.domain.article.model.Article;
 import github.devparkge.realworld.domain.user.model.User;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record Comment(
         int id,
         LocalDateTime createdAt,
@@ -13,4 +15,26 @@ public record Comment(
         User author,
         Article article
 ) {
+    public static Comment create(User author, Article article, String body) {
+        LocalDateTime now = LocalDateTime.now();
+        return Comment.builder()
+                .id(-1)
+                .createdAt(now)
+                .updatedAt(now)
+                .body(body)
+                .author(author)
+                .article(article)
+                .build();
+    }
+
+    public Comment updateId(int id) {
+        return Comment.builder()
+                .id(id)
+                .createdAt(createdAt())
+                .updatedAt(updatedAt())
+                .body(body())
+                .author(author())
+                .article(article())
+                .build();
+    }
 }
