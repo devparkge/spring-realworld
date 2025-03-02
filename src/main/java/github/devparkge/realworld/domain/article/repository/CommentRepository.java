fe0860b200cc551fa4b8eedc1e70e7861d9859ec
PARKGE <phg5459@naver.com>
2025-03-02 21:58:40 +0900

    save 메소드 추가

--- src/main/java/github/devparkge/realworld/domain/article/repository/CommentRepository.java
+++ src/main/java/github/devparkge/realworld/domain/article/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package github.devparkge.realworld.domain.article.repository;
 
+import github.devparkge.realworld.domain.article.model.Comment;
+
 public interface CommentRepository {
+    Comment save(Comment comment);
 }
--- src/main/java/github/devparkge/realworld/infrastructure/article/repository/InMemoryCommentRepository.java
+++ src/main/java/github/devparkge/realworld/infrastructure/article/repository/InMemoryCommentRepository.java
@@ -1,10 +1,21 @@
 package github.devparkge.realworld.infrastructure.article.repository;
 
+import github.devparkge.realworld.domain.article.model.Comment;
 import github.devparkge.realworld.domain.article.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class InMemoryCommentRepository implements CommentRepository {
+    private List<Comment> comments = new ArrayList<>();
+
+    @Override
+    public Comment save(Comment comment) {
+        comments.add(comment);
+        return comment;
+    }
 }
