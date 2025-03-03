4d21533b14356b37fb7a69dc53d88b3da51cb473
PARKGE <phg5459@naver.com>
2025-03-03 21:46:34 +0900

    Optional로 타입변경

--- src/main/java/github/devparkge/realworld/domain/comment/repository/CommentReadRepository.java
+++ src/main/java/github/devparkge/realworld/domain/comment/repository/CommentReadRepository.java
@@ -4,7 +4,9 @@ import github.devparkge.realworld.domain.article.model.Slug;
 import github.devparkge.realworld.domain.comment.model.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentReadRepository {
     List<Comment> findByArticleSlug(Slug slug);
+    Optional<Comment> findByCommentId(int commentId);
 }
