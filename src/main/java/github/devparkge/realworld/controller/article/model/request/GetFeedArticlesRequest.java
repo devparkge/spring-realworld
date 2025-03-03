ba1196e009726fb3dddfe310389965d1a0d3efac
PARKGE <phg5459@naver.com>
2025-03-03 22:29:54 +0900

    limit, offset default 값 부여

--- src/main/java/github/devparkge/realworld/controller/article/model/request/GetFeedArticlesRequest.java
+++ src/main/java/github/devparkge/realworld/controller/article/model/request/GetFeedArticlesRequest.java
@@ -1,7 +1,18 @@
 package github.devparkge.realworld.controller.article.model.request;
 
 public record GetFeedArticlesRequest(
-        int limit,
-        int offset
+        Integer limit,
+        Integer offset
 ) {
+    @Override
+    public Integer limit() {
+        if(limit == null) return 20;
+        return limit;
+    }
+
+    @Override
+    public Integer offset() {
+        if(offset == null) return 0;
+        return offset;
+    }
 }
