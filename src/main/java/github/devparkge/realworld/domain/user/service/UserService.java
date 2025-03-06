42f4d5befbdc33519d840a9fdf8c43da12823cd0
PARKGE <phg5459@naver.com>
2025-03-06 19:49:20 +0900

    사용 메소드 변경 updateUser > saveUser

--- src/main/java/github/devparkge/realworld/domain/user/service/UserService.java
+++ src/main/java/github/devparkge/realworld/domain/user/service/UserService.java
@@ -30,6 +30,6 @@ public class UserService {
                 .map(user -> user.update(uuid, email, username, password, bio, image))
                 .orElseThrow(() -> new UUIDNotFoundException("존재하지 않는 아이디입니다."));
 
-        return userRepository.updateUser(updateUser);
+        return userRepository.saveUser(updateUser);
     }
 }
