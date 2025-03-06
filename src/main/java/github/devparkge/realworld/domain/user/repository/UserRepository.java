ded25cb13b1146c2c4600bf655c4648c188d67ef
PARKGE <phg5459@naver.com>
2025-03-06 19:53:01 +0900

    updateUser 메소드 제거

--- src/main/java/github/devparkge/realworld/domain/user/repository/UserRepository.java
+++ src/main/java/github/devparkge/realworld/domain/user/repository/UserRepository.java
@@ -2,13 +2,10 @@ package github.devparkge.realworld.domain.user.repository;
 
 import github.devparkge.realworld.domain.user.model.User;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends UserReadRepository {
     User saveUser(User saveUser);
-
-    User updateUser(User updateUser);
 
     void follow(UUID followerId, UUID followeeId);
 
