1c66f0dc9fbd227e9e563630c2ee64b81453b513
PARKGE <phg5459@naver.com>
2025-03-06 19:54:15 +0900

    InMemoryUserRepository > UserRepositoryImpl rename 로직 수정

---
+++ src/main/java/github/devparkge/realworld/infrastructure/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,38 @@
+package github.devparkge.realworld.infrastructure.user.repository;
+
+import github.devparkge.realworld.infrastructure.user.dao.FollowDao;
+import github.devparkge.realworld.infrastructure.user.dao.UserDao;
+import github.devparkge.realworld.infrastructure.user.model.FollowEntity;
+import github.devparkge.realworld.domain.user.model.User;
+import github.devparkge.realworld.domain.user.repository.UserRepository;
+import github.devparkge.realworld.infrastructure.user.model.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public class UserRepositoryImpl extends UserReadRepositoryImpl implements UserRepository {
+
+    public UserRepositoryImpl(UserDao userDao, FollowDao followDao) {
+        super(userDao, followDao);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userDao.save(UserEntity.from(user)).toDomain();
+    }
+
+    @Override
+    public void follow(UUID followerId, UUID followeeId) {
+        UserEntity followerEntity = resolveUserEntity(followerId);
+        UserEntity followeeEntity = resolveUserEntity(followeeId);
+        followDao.save(FollowEntity.of(followeeEntity, followerEntity));
+    }
+
+    @Override
+    public void unFollow(UUID followerId, UUID followeeId) {
+        UserEntity followerEntity = resolveUserEntity(followerId);
+        UserEntity followeeEntity = resolveUserEntity(followeeId);
+        followDao.deleteByFolloweeEntityAndFollowerEntity(followeeEntity, followerEntity);
+    }
+}
