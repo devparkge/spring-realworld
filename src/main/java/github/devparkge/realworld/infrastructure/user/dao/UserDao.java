a54a3525224798e195eb49f598297934c04d82ef
PARKGE <phg5459@naver.com>
2025-03-06 19:48:44 +0900

    메소드 추가

--- src/main/java/github/devparkge/realworld/infrastructure/user/dao/UserDao.java
+++ src/main/java/github/devparkge/realworld/infrastructure/user/dao/UserDao.java
@@ -3,7 +3,11 @@ package github.devparkge.realworld.infrastructure.user.dao;
 import github.devparkge.realworld.infrastructure.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserDao extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findById(UUID userId);
+    Optional<UserEntity> findByUsername(String username);
 }
