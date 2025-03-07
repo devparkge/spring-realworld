c6e7ebd46a65a4aac9246facd2566619eacc9735
PARKGE <phg5459@naver.com>
2025-03-07 23:57:44 +0900

    타입 변경

--- src/main/java/github/devparkge/realworld/domain/article/model/Tag.java
+++ src/main/java/github/devparkge/realworld/domain/article/model/Tag.java
@@ -1,16 +1,17 @@
 package github.devparkge.realworld.domain.article.model;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record Tag (
         UUID tagId,
         String tagName,
-        LocalDateTime createdAt,
-        LocalDateTime updatedAt
+        Instant createdAt,
+        Instant updatedAt
 ) {
     public static Tag create(String tagName) {
-        LocalDateTime now = LocalDateTime.now();
+        Instant now = Instant.now();
         return new Tag(
                 UUID.randomUUID(),
                 tagName,
